34d619daf97f63c3952d972e94c93ba3a8c6897c
Bassam El-Naggar <89227338+BassGI1@users.noreply.github.com>
2022-02-05 17:02:42 -0500

    Finished

---
+++ main.js
@@ -0,0 +1,175 @@
+function funcs(){
+
+    x = document.getElementById('Ranch');
+    var y = document.getElementById('RanchRemoved');
+
+    if (x.checked){
+        y.style.display = "block";
+        ran = true;
+    }
+    else {
+        y.style.display = "none";
+    }
+
+}
+
+function func(){
+
+    che = document.getElementById('Marinara');
+    var pic = document.getElementById('TomatoSauceRemoved');
+
+    if (che.checked){
+        pic.style.display = "block";
+        mar = true;
+    }
+    else {
+        pic.style.display = "none";
+    }
+
+}
+
+function Func(){
+
+    mozz = document.getElementById('Mozzarella');
+    var dis = document.getElementById('MozzarellaRemoved');
+
+    if (mozz.checked){
+        dis.style.display = "block";
+    }
+    else {
+        dis.style.display = "none";
+    }
+
+}
+
+function Fun(){
+
+    ched = document.getElementById('Cheddar');
+    var disp = document.getElementById('CheddarRemoved');
+
+    if (ched.checked){
+        disp.style.display = "block";
+    }
+    else {
+        disp.style.display = "none";
+    }
+
+}
+
+function Fnc(){
+
+    Prov = document.getElementById('Provolone');
+    var pic = document.getElementById('ProvoloneRemoved');
+
+    if (Prov.checked){
+        pic.style.display = "block";
+    }
+    else {
+        pic.style.display = "none";
+    }
+
+}
+
+function fn(){
+
+    Mush = document.getElementById('Mushroom');
+    var pic = document.getElementById('MushroomRemoved');
+
+    if (Mush.checked){
+        pic.style.display = "block";
+        mush = true;
+    }
+    else {
+        pic.style.display = "none";
+    }
+
+}
+
+function f(){
+
+    Oli = document.getElementById('Olive');
+    var pic = document.getElementById('OliveRemoved');
+
+    if (Oli.checked){
+        pic.style.display = "block";
+        oli = true;
+    }
+    else {
+        pic.style.display = "none";
+    }
+
+}
+
+function fa() {
+
+    Oni = document.getElementById('Onion');
+    var pic = document.getElementById('OnionRemoved');
+
+    if (Oni.checked){
+        pic.style.display = "block";
+        oni = true;
+    }
+    else {
+        pic.style.display = "none";
+    } 
+
+}
+
+function fq() {
+
+    Pep = document.getElementById('Pepper');
+    var pic = document.getElementById('PepperRemoved');
+
+    if (Pep.checked){
+        pic.style.display = "block";
+        pep = true;
+    }
+    else {
+        pic.style.display = "none";
+    } 
+
+}
+
+function ft() {
+
+    Tom = document.getElementById('Tomato');
+    var pic = document.getElementById('TomatoRemoved');
+
+    if (Tom.checked){
+        pic.style.display = "block";
+        tom = true;
+    }
+    else {
+        pic.style.display = "none";
+    } 
+
+}
+
+function fc() {
+
+    Chi = document.getElementById('Chicken');
+    var pic = document.getElementById('ChickenRemoved');
+
+    if (Chi.checked){
+        pic.style.display = "block";
+        chi = true;
+    }
+    else {
+        pic.style.display = "none";
+    } 
+
+}
+
+function fg() {
+
+    Pepp = document.getElementById('Pepperoni');
+    var pic = document.getElementById('PepperoniRemoved');
+
+    if (Pepp.checked){
+        pic.style.display = "block";
+        pepp = true;
+    }
+    else {
+        pic.style.display = "none";
+    } 
+}
